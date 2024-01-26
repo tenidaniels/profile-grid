ad6a6dcb2be0c68d796859b53d0979d584271aa2
Oluwateniola DANIEL Olubowale <52575436+tenidaniels@users.noreply.github.com>
2024-01-26 15:27:57 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Star from "./Star"
 
 export default function App() {
     const [contact, setContact] = React.useState({
@@ -6,13 +7,11 @@ export default function App() {
         lastName: "Doe",
         phone: "+1 (719) 555-1212",
         email: "itsmyrealname@example.com",
-        isFavorite: false
+        isFavorite: true
     })
     
-    let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
-    
     function toggleFavorite() {
-                setContact(prevContact => ({
+        setContact(prevContact => ({
             ...prevContact,
             isFavorite: !prevContact.isFavorite
         }))
@@ -23,11 +22,7 @@ export default function App() {
             <article className="card">
                 <img src="./images/user.png" className="card--image" />
                 <div className="card--info">
-                    <img 
-                        src={`../images/${starIcon}`} 
-                        className="card--favorite"
-                        onClick={toggleFavorite}
-                    />
+                    <Star isFilled={contact.isFavorite} handleClick={toggleFavorite} />
                     <h2 className="card--name">
                         {contact.firstName} {contact.lastName}
                     </h2>
@@ -39,3 +34,4 @@ export default function App() {
         </main>
     )
 }
+
