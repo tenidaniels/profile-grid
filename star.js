08a1b770ddc10c30482a6bd60afd13c4d69232ea
Oluwateniola DANIEL Olubowale <52575436+tenidaniels@users.noreply.github.com>
2024-01-26 15:31:40 +0100

    Update star.js

--- star.js
+++ star.js
@@ -2,11 +2,20 @@ import React from "react"
 
 export default function Star(props) {
     const starIcon = props.isFilled ? "star-filled.png" : "star-empty.png"
+    const buttonLabel = props.isFilled ? "Unmark as favorite" : "Mark as favorite"
+    
     return (
-        <img 
-            src={`../images/${starIcon}`} 
-            className="card--favorite"
+        <button
             onClick={props.handleClick}
-        />
+            aria-label={buttonLabel}
+            aria-pressed={props.isFilled}
+            className="card--favorite-button"
+        >
+            <img
+                src={`../images/${starIcon}`}
+                alt="star icon."
+                className="card--favorite"
+            />
+        </button>
     )
 }
